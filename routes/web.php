d009a6cb6500e7c8fb9f8e0f81ce4568d12e0fd8
Richard Ottinger <richard.ottinger@gmail.com>
2020-08-03 05:50:26 -0500

    add auth

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,13 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Auth::routes(['register' => false]);
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
