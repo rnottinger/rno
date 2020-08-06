cd5c0185e70fe6c1de22524984fd10c9f2229435
Richard Ottinger <richard.ottinger@gmail.com>
2020-08-06 05:31:21 -0500

    remove registration

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Auth::routes(['register' => false]);
-Auth::routes();
+Auth::routes(['register' => false]);
+// Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
