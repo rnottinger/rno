c1f825bf30d06dc4a370f9159096acc9abf1e8fc
Richard Ottinger <richard.ottinger@gmail.com>
2020-08-06 21:09:30 -0500

    dispatch a job onto the queue

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::get('/', function () {
+    dispatch(function () {
+        logger('Hello Dude!');
+    });
+    // ->delay(now()->addMinutes(1));
     return view('welcome');
 });
 
